d647f551fbf89bfb869c014fac82beec2873cd14
Kyra Henningson <kyrahenningson@hotmail.com>
2022-08-25 00:25:57 +0000

    cleaned up excessive pseudocode

--- eqObjects.js
+++ eqObjects.js
@@ -21,13 +21,10 @@ const eqArrays = function(arr1, arr2) {
   };
 
 // Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
 //Do the objects have the same number of keys?
 const obj1keys = Object.keys(object1);
-//console.log('obj1keys', obj1keys);
 const obj2keys = Object.keys(object2);
-//console.log('obj2keys', obj2keys);
 if (obj1keys.length !== obj2keys.length) {
   return false;
 }
@@ -39,16 +36,11 @@ for (const key of obj1keys) {
   //Use Array.isArray against both values to see if array comparisons need to be made 
   //If both values arrays - pass to eqArrays
   if (Array.isArray(val1) && Array.isArray(val2)) {
-    //console.log('Val1 an array?', Array.isArray(val1));
-    //console.log('Val2 an array?', Array.isArray(val2));
    return eqArrays(val1, val2);
   }
   //Otherwise else assume primitives and continue to compare the two values 
-  //console.log('key', key);
   //Compare both objects' values
    if (val1 !== val2) {
-    //console.log('Value of obj1keys', val1);
-    //console.log('Value of obj2keys', val2);
     return false;
   }
 }
@@ -58,20 +50,17 @@ return true;
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-
 const abc = { a: "1", b: "2", c: "3" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+
+eqObjects(ab, ba); // => true
 eqObjects(ab, abc); // => false
+eqObjects(cd, dc); // => true
+eqObjects(cd, cd2); // => false
 
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
-
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
