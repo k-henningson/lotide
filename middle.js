144b64be6860e2c2ed74f78e2501a000c2e4cbe6
Kyra Henningson <kyrahenningson@hotmail.com>
2022-08-26 00:09:28 +0000

    updated middle js to include better assertEqual function and test cases

--- middle.js
+++ middle.js
@@ -12,25 +12,27 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function(arr1, arr2) {
-  const result = eqArrays(arr1, arr2);
-  assertEqual(result, true);
+
+const assertArraysEqual = function(actual, expected) {
+  const result = eqArrays(actual, expected);
+  if (result) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 //Middle will return an array with only the middle element(s)
 const middle = function(arr) {
-  //New array
   let midArr = [];
   //If array is less than 2 elements, return an empty array
   if (arr.length <= 2) {
-    console.log('Less than 2', midArr);
     return midArr;
   }
   //If array odd number elements, return single middle element
   if (arr.length % 2 !== 0) {
     let odd = arr[Math.floor((arr.length - 1) / 2)];
     midArr.push(odd);
-    console.log('Odd', midArr);
     return midArr;
   }
   //If array even number elements, return two middle elements
@@ -38,7 +40,6 @@ const middle = function(arr) {
     let middle1 = arr[Math.floor(arr.length / 2) - 1];
     let middle2 = arr[Math.floor(arr.length / 2)];
     midArr.push(middle1, middle2);
-    console.log('Even', midArr);
     return midArr;
   }
 };
@@ -51,3 +52,8 @@ middle([1, 2, 3, 4, 5]); // => [3]
 
 middle([1, 2, 3, 4]); // => [2, 3]
 middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+assertArraysEqual(middle([1]), []); // => PASS 
+assertArraysEqual(middle([1, 2, 3]), [2]); // => PASS 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => PASS
+assertArraysEqual(middle([1, 2, 3, 4]), [2]); // => FAIL
