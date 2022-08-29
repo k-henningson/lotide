0bf36dbf47e98d21c6419315dab26645980b6e87
Kyra Henningson <kyrahenningson@hotmail.com>
2022-08-29 04:28:14 +0000

    removed test code, required & exported

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual('Hi', 'Hi');
-assertEqual('Kyra', 'James');
-assertEqual(12, 12);
-assertEqual(10, 12);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,3 +1,5 @@
+const assertEqual = require('./assertEqual');
+
 const head = function(arr) {
   if (arr.length === 0) {
     return undefined;
@@ -5,23 +7,4 @@ const head = function(arr) {
   return arr[0];
 };
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual('Hi', 'Hi');
-// assertEqual('Kyra', 'James');
-// assertEqual(12, 12);
-// assertEqual(10, 12);
+module.exports = head;
