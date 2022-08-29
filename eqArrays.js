a066b4a94c8dd0adc7fe50228e9408522bfdd028
Kyra Henningson <kyrahenningson@hotmail.com>
2022-08-29 15:58:12 +0000

    require & module export for eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   //Do arrays have same length
   if (arr1.length !== arr2.length) {
@@ -20,13 +12,10 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+module.exports = eqArrays;
+
 // eqArrays([1, 2, 3], [1, 2, 3]) // => true
 // eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
 // eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 // eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
